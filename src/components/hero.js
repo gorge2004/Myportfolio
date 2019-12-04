e0114774139eb97b13c088a0296536b3df56b25d
Jorge espinoza <gorge2004@gmail.com>
2019-12-04 11:50:29 -0300

    adding animation, styles, background and  working with redux

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,32 +1,85 @@
 import React from 'react';
 
+import { connect } from "react-redux";
 
 class Hero extends React.Component{
   constructor(props) {
     super(props)
-      let h1First;
+    console.log("properties",props.title);
+      let h1Texts, textMid, textEnd;
   }
 
-  componentDidMount(){
-    console.log(document.getElementsByTagName('h1'));
+  componentDidUpdate(){
+    this.fragmentingTitle();
   }
+
+
+    componentDidMount(){
+      this.fragmentingTitle();
+  }
+
+  fragmentingTitle = () => {
+    this.h1Texts = document.querySelectorAll(".h1-hero");
+    this.textMid = document.querySelector(".aux-cut-letter-mid");
+    this.textEnd = document.querySelector(".aux-cut-letter-end");
+    this.h1Texts.forEach( h1 => this.setWidth( h1, ""));
+
+    const LENGTH_TEXT = this.props.title.length ;
+    const WIDTH_BY_CHARACTER  = this.getRound(this.h1Texts[0].offsetWidth / LENGTH_TEXT);
+    const CHARACTERS_BY_SECTION = this.getRound(LENGTH_TEXT / 3);
+    const WIDTH_BY_SECTION = this.getRound(WIDTH_BY_CHARACTER * CHARACTERS_BY_SECTION);
+
+    const LEFT_TO_MID_SECTION = this.getRound(WIDTH_BY_SECTION * -1)+ this.getUnit();
+    const LEFT_TO_END_SECTION = this.getRound(WIDTH_BY_SECTION * -2)+ this.getUnit();
+    const MIN_HEIGHT = this.h1Texts[0].offsetHeight;
+    console.log("height",MIN_HEIGHT);
+
+
+     this.h1Texts.forEach( h1 => this.setWidth( h1,  WIDTH_BY_SECTION+ this.getUnit() ));
+     this.setMinHeight(this.h1Texts[1],MIN_HEIGHT+ this.getUnit());
+     this.setMinHeight(this.h1Texts[2],MIN_HEIGHT+ this.getUnit() );
+
+     this.setLeft(this.textMid,  LEFT_TO_MID_SECTION);
+     this.setLeft(this.textEnd,  LEFT_TO_END_SECTION);
+  }
+
+   getRound = (value) =>{
+
+    return value.toFixed(2);
+
+  }
+   getUnit = (unit = "px") =>{
+    return unit
+  }
+  setWidth = (element, length) =>{
+      element.style.width = length;
+  }
+  setMinHeight = (element, length) =>{
+      element.style.minHeight = length;
+  }
+  setLeft = (element, length) => {
+    element.style.left = length ;
+
+  }
+
   render(){
-    this.load();
+    console.log("render");
       return (
-        <div className='hero'>
-            <h1  >
-              Jorge
+        <div className='hero'onClick={ () => this.props.dispatch({type: 'ES'}) } >
+            <h1  className="h1-hero"  onClick={(evt) => console.log(evt.target.width) }>
+            { this.props.title }
             </h1>
-              <h1>
-              <p className="aux-cut-letter-mid">
-              Jorge
-              </p>
+              <h1 className="h1-hero">
+                <p className="aux-cut-letter-mid">
+                { this.props.title }
+
+                </p>
               </h1>
 
-              <h1>
-              <p className="aux-cut-letter-end">
-              Jorge
-              </p>
+              <h1 className="h1-hero">
+                <p className="aux-cut-letter-end">
+                { this.props.title }
+                </p>
               </h1>
 
         </div>
@@ -35,4 +88,10 @@ class Hero extends React.Component{
 
   }
 }
-export default Hero;
+const mapStateToPros = (state) =>{
+  return {...state.HERO};
+} ;
+const mapDispatchToProps = (dispatch) => {
+  return ({dispatch: (action) => dispatch(action)});
+}
+export default connect(mapStateToPros, mapDispatchToProps)(Hero) ;
