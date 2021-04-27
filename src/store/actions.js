5227daba37af3431ca308409c559600125a819fe
Jorge espinoza <gorge2004@gmail.com>
2021-04-27 16:56:07 -0300

    fixing text

--- src/store/actions.js
+++ src/store/actions.js
@@ -127,60 +127,60 @@ export const EN = {
   content: {
     Jobs: [
       {
-        title: "Analista de sistema (Intern)- PHP Developer",
+        title: "Systems Engineer (Intern)- PHP Developer",
         location: "Maturin, Venezuela",
         duration: "Jan. 2016 – Jul. 2017",
         description:
-          "Reingeniería de software de sistema web PHP y MySQL."+
-          " Lenguaje  PHP para modificar funcionalidades ya existente y añadir las requeridas."
-          +" Utilizando Php 5.0, Mysql, BoostrapCss 3",
+          "Software reengineering of technical support, technologies PHP y MySQL."+
+          "this job was modified systems features  y add new ones."
+          +" Using Php 5.0, Mysql, BoostrapCss 3",
       },
       {
-        title: "Web Developer( Freelance)",
+        title: "Web Developer Aquitupides( Freelance)",
         location: "Maturin, Venezuela",
         duration: "May. 2017 – Jul. 2017",
         description:
-          "Desarrollo de software, añadiendo nuevas funcionalidades al sistema. Desarrollado con CakePHP y PostgreSQL."
-          +" Utilizando cakephp 2.3, y materializeCss",
+          "Developing new features that lets user post articles on web site. Technologies used: cakephp 2.3,  materializeCss and PostgreSQL."
+          ,
       },
       {
-        title: "Desarrollador web( Freelance)",
+        title: "Web Developer( Freelance)",
         location: "MonkuApp, Peru ( Freelance)",
         duration: "Jan. 2019 – Apr 2019",
         description:
-          "Desarrollo en distintos sitios web."
-          +" Usando distintas tecnologías como Koa(NodeJs), PUG, JavaScript, Jquery.",
+          "used to modify several web sites, using technologies As  Koa(NodeJs), PUG, JavaScript, Jquery."
       },
       {
-        title: "Desarrollador web( Freelance)",
+        title: "Web Developer( Freelance)",
         location: "Bucares, Francia ( Freelance)",
         duration: "Jun. 2019 - Jul. 2019",
         description:
-          "Apoyo en el desarrollo de una Tienda prestaShop, y desarrollo de un plugin para tienda realizada en  Wordpress.",
+          " Desarrollo of a plugin to a wordpress shop.",
       },
       {
-        title: "Desarrollador Salesforce",
+        title: "Salesforce Developer",
         location: "Xappia, Argentina",
         duration: "Jan. 2020- Nov. 2020",
         description:
-          "Desarrollo con lenguaje Apex, distinto requerimientos en distintos projectos.",
+          "Working in several projects and developing distinct requeriments in each one. Apex, Js, Html",
       },
       {
-        title: "Desarrollador Web",
+        title: "Web Developer",
         location: "Nectica, Argentina",
         duration: "Nov. 2020- Presente",
         description:
-          "Desarrollo  de distintos sistemas administrativos . Realizados con laravel. Modificacion de sitios ya existente con php y Javascript",
+          "Developing  Administration systems and modified some sites using Php and Javascript. Develope by laravel.",
       },
     ],
   },
   skill: {
-    description: ` EN Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, `,
+    description: ` I have around 2 years experienced as a Web developer, and have used different technologies in special PHP, Javascript and Mysql. 
+    i have worked  with technologies as laravel, cakephp, react, react native , Koajs, 
+    BootstrapCss, MaterialiceCss , both in  companies projects and personals project. 
+    i took several course with diffents programming languagues as java, C# and netcore,
+    and have learned myseft a bite about React.js, redux, react native, vuejs , nodejs, becuase 
+    these technologies are actually popular in this jobs world. i think myselft as full responsibility person who can learn so fast and reach goals.
+    my hobbies is watching movies and like to learn about crypto Currencies to save way and awesome tenchologies what use on it. `,
     json_skill: [
       "javascript",
       "php",
